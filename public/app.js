81b9ffa3caf82f9d9c2321f929de04ef112a626f
shyssirixius <ivmikvik@gmail.com>
2016-08-12 01:07:31 +0300

    start adding pizzas

--- public/app.js
+++ public/app.js
@@ -2,11 +2,11 @@
 
 	function something() {
 
-		var x = window.localStorage.getItem('bbb');
+		var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-		x = x * 1 + 1;
+		x = x * 1 + 1; //string to integer x = x + 1
 
-		window.localStorage.setItem('bbb',x);
+		window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 		alert(x);
 
