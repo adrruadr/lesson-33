bbe9f2af09c3baf2e94598e91c38638cc50bb437
shyssirixius <ivmikvik@gmail.com>
2016-08-12 01:46:15 +0300

    update add_to_cart

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 
 
-	function something() {
-
+	function something() 
+	{
 		var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
 		x = x * 1 + 1; //string to integer x = x + 1
@@ -9,11 +9,15 @@
 		window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 		alert(x);
-
 	}
 
-	function add_to_cart(id) {
-		alert('You added pizza with id:' + id);
-	}
+	function add_to_cart(id) 
+	{
+		var key = 'product_' + id;
+		
+		var x = window.localStorage.getItem(key);
+		x = x * 1 + 1;
+		window.localStorage.setItem(key, x);
+ 	}
 
 	
