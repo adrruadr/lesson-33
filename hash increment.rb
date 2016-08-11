cc7a79bb031c362f072c36738cf755e191a57cf5
shyssirixius <ivmikvik@gmail.com>
2016-08-12 01:49:00 +0300

    test show of total in cart

--- hash increment.rb
+++ hash increment.rb
@@ -13,6 +13,17 @@ x = x + n # increased by n
 hh[id] = x # set has value
 
 puts hh.inspect
+
+#calculated total numbers of cart
+total = 0
+hh.each do |key,value|
+		total = total + value
+	end
+
+	#puts total variable
+	puts "Total items in cart #{total}"
+
+
 puts '---------------'
 
 end
